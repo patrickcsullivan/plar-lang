5b7588f3144c17568b9860c805ed0530dfa8d24e
Patrick Sullivan <patrick.sullivan@hey.com>
2022-03-17 00:33:31 -0500

    chapter 1

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -1,12 +1,10 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Use newtype instead of data" #-}
 module Chapter1
   ( someFunc,
     Expression (..),
     simplify,
     testExpr,
-    lex,
+    testStr,
+    defaultParser,
   )
 where
 
@@ -20,9 +18,11 @@ data Expression
   | Const Int
   | Add Expression Expression
   | Mul Expression Expression
-  deriving (Show)
 
-data Error = Error String
+instance Show Expression where
+  show = prettyPrint 0
+
+newtype Error = Error String deriving (Show)
 
 simplify1 :: Expression -> Expression
 simplify1 expr = case expr of
@@ -45,37 +45,99 @@ simplify expression = case expression of
 testExpr :: Expression
 testExpr = Add (Mul (Const 0) (Var "x")) (Var "y")
 
+testStr :: String
+testStr = "(x1 + x2 + x3) * (1 + 2 + 3 * x + y)"
+
 -- LEXER
 
-space c = c `elem` " \t\n\r"
+type Token = String
 
-punctuation c = c `elem` "()[]{}"
+isSpace c = c `elem` " \t\n\r"
 
-symbolic c = c `elem` "~'!@#$%^&*-+=|\\:;<>.?/"
+isPunctuation c = c `elem` "()[]{}"
 
-numeric c = c `elem` "0123456789"
+isSymbol c = c `elem` "~'!@#$%^&*-+=|\\:;<>.?/"
 
-alphanumeric c = c `elem` "abcdefghijklmnopqrstuvwxyz_'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+isNumeric c = c `elem` "0123456789"
 
-lexWhile :: (Char -> Bool) -> String -> (String, String)
+isAlphanumeric c = c `elem` "abcdefghijklmnopqrstuvwxyz_'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+lexWhile :: (Char -> Bool) -> String -> (Token, String)
 lexWhile = span
 
-lex :: String -> [String]
+lex :: String -> [Token]
 lex input =
-  case snd $ lexWhile space input of
+  case snd $ lexWhile isSpace input of
     "" -> []
     c : cs ->
       let prop
-            | alphanumeric c = alphanumeric
-            | symbolic c = symbolic
+            | isAlphanumeric c = isAlphanumeric
+            | isSymbol c = isSymbol
             | otherwise = const False
        in let (toktl, rest) = lexWhile prop cs
            in (c : toktl) : lex rest
 
 -- PARSER
 
-parseAtom :: [String] -> Either Error Expression
+type TokenParser = [Token] -> Either Error (Expression, [Token])
+
+parseExpression :: TokenParser
+parseExpression toks = do
+  (e1, toks1) <- parseProduct toks
+  case toks1 of
+    "+" : toks1 -> do
+      (e2, toks2) <- parseExpression toks1
+      return (Add e1 e2, toks2)
+    _ -> return (e1, toks1)
+
+parseProduct :: TokenParser
+parseProduct toks = do
+  (e1, toks1) <- parseAtom toks
+  case toks1 of
+    "*" : toks1 -> do
+      (e2, toks2) <- parseProduct toks1
+      return (Mul e1 e2, toks2)
+    _ -> return (e1, toks1)
+
+parseAtom :: TokenParser
 parseAtom toks =
   case toks of
     [] -> Left $ Error "Expected an expression at end of input"
-    _ -> undefined
+    "(" : toks1 -> do
+      (e2, toks2) <- parseExpression toks1
+      case toks2 of
+        ")" : toks2 -> return (e2, toks2)
+        _ -> Left $ Error "Expected closing bracket"
+    tok : toks1 ->
+      if all isNumeric tok
+        then Right (Const (read tok), toks1)
+        else Right (Var tok, toks1)
+
+makeParser :: TokenParser -> String -> Either Error Expression
+makeParser parse input = do
+  let toks = lex input
+  (expr, toksRest) <- parse toks
+  case toksRest of
+    [] -> return expr
+    _ -> Left $ Error "Unparsed input"
+
+defaultParser :: String -> Either Error Expression
+defaultParser = makeParser parseExpression
+
+-- PRETTY PRINTER
+
+prettyPrint :: Int -> Expression -> String
+prettyPrint precidence expression =
+  case expression of
+    Var s -> s
+    Const n -> show n
+    Add e1 e2 ->
+      let s = prettyPrint 3 e1 ++ " + " ++ prettyPrint 2 e2
+       in if precidence > 2
+            then "(" ++ s ++ ")"
+            else s
+    Mul e1 e2 ->
+      let s = prettyPrint 5 e1 ++ " * " ++ prettyPrint 4 e2
+       in if precidence > 4
+            then "(" ++ s ++ ")"
+            else s
